223dbcbed0345af11f0b7d7744f3558d9528a984
Giovanni Andrade Nespoli <ggnespoli@gmail.com>
2023-04-03 21:28:49 -0300

    add errors

--- silFazendoArte/js/calcula-total.js
+++ silFazendoArte/js/calcula-total.js
@@ -5,12 +5,12 @@ titulo.textContent = "Lista de Encomendas";
 //Captura os dados de todos os clientes
 var clientes = document.querySelectorAll(".cliente");
 
-for (var i=0; i < clientes.length; i++){
-    
+for (var i = 0; i < clientes.length; i++) {
+
     /*================================================================
         ROTINA DE CÁLCULO DO VALOR TOTAL POR ENCOMENDA
     ==================================================================*/
-    
+
     //Captura a QUANTIDADE encomendada por cada cliente
     var qtde = clientes[i].querySelector(".info-qtde").textContent;
 
@@ -18,39 +18,37 @@ for (var i=0; i < clientes.length; i++){
     var unitario = clientes[i].querySelector(".info-valor").textContent;
 
     //Verifica se a QUANTIDADE recebida é válida
-    if (!validaQtde(qtde)){
-        clientes[i].querySelector(".info-total").textContent="Quantidade inválida!";
+    if (!validaQtde(qtde)) {
+        clientes[i].querySelector(".info-total").textContent = "Quantidade inválida!";
         //Colore a fonte dessa linha na cor vermelha
         clientes[i].style.color = "red";
-    }
-    else{
+    } else {
         //Verifica se o VALOR UNITÁRIO é válido
-        if (!validaUnitario(unitario)){
-            clientes[i].querySelector(".info-total").textContent="Valor unitário é inválido!";
+        if (!validaUnitario(unitario)) {
+            clientes[i].querySelector(".info-total").textContent = "Valor unitário é inválido!";
             //Colore o fundo dessa linha na cor light coral
             clientes[i].style.backgroundColor = "lightcoral";
-        }
-        else{
-                //Formata o VALOR UNITÁRIO
-                clientes[i].querySelector(".info-valor").textContent = formataValor(unitario);
+        } else {
+            //Formata o VALOR UNITÁRIO
+            clientes[i].querySelector(".info-valor").textContent = formataValor(unitario);
 
             //Calcula o VALOR TOTAL e exibe na tabela
-            clientes[i].querySelector(".info-total").textContent= calculaTotal(qtde,unitario);
+            clientes[i].querySelector(".info-total").textContent = calculaTotal(qtde, unitario);
 
         }
     }
 }
 
 //Formatação de valor
-function formataValor(valor){
+function formataValor(valor) {
 
     var valor = parseFloat(valor);
 
-    return valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 }
 
 //Função para calcular o VALOR TOTAL
-function calculaTotal(qtde, unitario){
+function calculaTotal(qtde, unitario) {
     var total = 0;
 
     //Faz o cálculo do valor total
@@ -60,19 +58,37 @@ function calculaTotal(qtde, unitario){
 }
 
 //função que valida quantidade
-function validaQtde(qtde){
-    if(!isNaN(qtde) && (qtde)>0){
+function validaQtde(qtde) {
+    if (!isNaN(qtde) && (qtde) > 0) {
         return true;
-    }else{
+    } else {
+        return false;
+    }
+}
+
+//função que valida o nome
+function validaNome(nome) {
+    if (nome !== "") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//função que valida o produto
+function validaProduto(produto) {
+    if (produto !== "Selecione") {
+        return true;
+    } else {
         return false;
     }
 }
 
 //função que valida o valor unitario
-function validaUnitario (unitario){
-    if (!isNaN(unitario) && (unitario)>0){
+function validaUnitario(unitario) {
+    if (!isNaN(unitario) && (unitario) > 0) {
         return true;
-    }else{
+    } else {
         return false;
-    } 
+    }
 }
--- silFazendoArte/js/form.js
+++ silFazendoArte/js/form.js
@@ -1,18 +1,18 @@
 var botaoAdicionar = document.querySelector("#adicionar-encomenda");
 
-botaoAdicionar.addEventListener("click", function(event){
+botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
 
     //Captura o formulário
     var form = document.querySelector("#form-adiciona");
 
-    var validacao = validaEncomenda (obtemEncomenda(form));
+    var validacao = validaEncomenda(obtemEncomenda(form));
 
-    if (validacao.length > 0){
+    if (validacao.length > 0) {
         exibeMensagensErro(validacao);
         return;
     }
- 
+
     //Captura a tabela de encomendas
 
     var tabela = document.querySelector("#tabela-clientes");
@@ -29,7 +29,7 @@ botaoAdicionar.addEventListener("click", function(event){
 
 
 //Captura os dados da encomenda
-function obtemEncomenda(form){
+function obtemEncomenda(form) {
 
     var encomenda = {
         nome: form.nome.value,
@@ -41,49 +41,67 @@ function obtemEncomenda(form){
     return encomenda;
 }
 
-function montaTr(encomenda){
+function montaTr(encomenda) {
     //Cria a nova linha da tabela
     var encomendaTr = document.createElement("tr");
-  
+
     //Monta a nova linha
     encomendaTr.appendChild(montaTd(encomenda.nome));
     encomendaTr.appendChild(montaTd(encomenda.produto));
     encomendaTr.appendChild(montaTd(encomenda.qtde));
     encomendaTr.appendChild(montaTd(formataValor(encomenda.unitario)));
-    encomendaTr.appendChild(montaTd(calculaTotal(encomenda.qtde,encomenda.unitario)));
+    encomendaTr.appendChild(montaTd(calculaTotal(encomenda.qtde, encomenda.unitario)));
 
     return encomendaTr;
 }
 
-function montaTd(dados){
-    
+function montaTd(dados) {
+
     var td = document.createElement("td");
     td.textContent = dados;
 
     return td;
 }
 
-function validaEncomenda(encomenda){
-    var erros = [];
-    
-    if (!validaQtde(encomenda.qtde) ){
+var erros = [];
+
+function validaEncomenda(encomenda) {
+
+    if (erros.length > 0) {
+        erros.length = 0;
+    }
+
+    if (!validaNome(encomenda.nome)) {
+        erros.push("Nome inválido");
+    }
+
+    if (!validaProduto(encomenda.produto)) {
+        erros.push("Produto inválido");
+    }
+
+    if (!validaQtde(encomenda.qtde)) {
         erros.push("Quantidade inválida");
     }
 
-    if (!validaUnitario(encomenda.unitario) ){
+    if (!validaUnitario(encomenda.unitario)) {
         erros.push("Valor unitário inválido");
     }
-return erros
+    return erros
 }
 
 //função para exibir os erros de preenchimento de formulario
 
-function exibeMensagensErro(erros){
-    var ul= document.querySelector("#mensagens-erro");
-        erros.forEach(function(erro){
-            var li = document.createElement("li");
-            li.textContent = erro;
-            ul.appendChild(li);
-        });
-}
+function exibeMensagensErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
 
+    if (erros.length > 1) {
+        ul.innerHTML = "";
+    }
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        li.style.color = "red";
+        ul.appendChild(li);
+    });
+}
